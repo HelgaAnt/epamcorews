61660fecfabe893ece46ea31afbb5a5828ae6079
HelgaAnt <antou@bk.ru>
2018-10-13 16:27:07 +0400

    HOL2. Add runtime-specific code

--- src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
+++ src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace EPAM.Core.ReportHelper.Tests
 {
@@ -11,9 +12,11 @@ namespace EPAM.Core.ReportHelper.Tests
     public class ReportNameHelperTests
     {
         [TestMethod]
+        [DynamicData("NormalizeFileNameTestData", DynamicDataSourceType.Property)]
+
         //[DataRow("Hello: my world", '_', "Hello_ my world")]
-        [DataRow("Hello my %world", '_', "Hello my %world")]
-        [DataRow("Hello my /world", '_', "Hello my _world")]
+        //[DataRow("Hello my %world", '_', "Hello my %world")]
+        //[DataRow("Hello my /world", '_', "Hello my _world")]
         public void NormalizeFileNameTest(string name, char repl, string expected)
         {
             var result = ReportNameHelper.NormalizeFileName(name, repl);
@@ -21,11 +24,32 @@ namespace EPAM.Core.ReportHelper.Tests
             result.Should().BeEquivalentTo(expected);
         }
 
-    
+
         [AssemblyInitialize]
         public static void Init(TestContext context)
         {
             //Thread.Sleep(30 * 1000);
+        }
+
+        static object[][] NormalizeFileNameTestData
+        {
+            get
+            {
+                object[][] data = new object[][]
+                {
+            new object[]{"Hello: my world", '_', "Hello_ my world"},
+            new object[]{"Hello my %world", '_', "Hello my %world"},
+            new object[]{"Hello my /world", '_', "Hello my _world"}
+                };
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    data[0][2] = "Hello: my world";
+                    data[1][2] = "Hello my %world";
+                    data[2][2] = "Hello my _world";
+                }
+                return data;
+            }
         }
 
     }
